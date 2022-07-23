c3588dab4749d12f28952c19d880f0b7ef9fa0dc
Jamee Jenkins <jbjenks27@gmail.com>
2022-07-23 16:36:51 -0600

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -64,13 +64,19 @@ with open(file_to_save, "w") as txt_file:
         vote_percentage = float(votes) / float(total_votes) * 100
         # Print each candidate, their voter count, and percentage to the
         # terminal.
-        # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
         # Determine winning vote count, winning percentage, and candidate.
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
             winning_candidate = candidate_name
             winning_percentage = vote_percentage
+
+        # Print each candidate, their voter count, and percentage to the terminal.
+        print(candidate_results)
+        #  Save the candidate results to our text file.
+        txt_file.write(candidate_results)
+
     # Print the winning candidates' results to the terminal.
     winning_candidate_summary = (
         f"-------------------------\n"
@@ -79,6 +85,7 @@ with open(file_to_save, "w") as txt_file:
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
 
-    # print(winning_candidate_summary)
+    # Save the winning candidate's name to the text file.
+    txt_file.write(winning_candidate_summary)
 
-
+ 
